6243497745c9d5f0dfc4fb29ef9901f59b92b19c
D. W <owl.robot@aol.com>
2021-08-16 17:47:18 -0400

    Create updatingmac.sh

---
+++ updatingmac.sh
@@ -0,0 +1,8 @@
+# Updatng Mac from command line.
+# Updates download and install faster from the terminal.
+
+sudo softwareupdate -l # To update your mac's system.
+
+sudo softwareupdate -i 'NAME' # To download and install a particular update.
+
+sudo softwareupdate -i -a # To install all available updates.
