7fa650abdb216bbe4c79eda9a4f5aa3403cd4303
D. W <owl.robot@aol.com>
2021-08-16 13:56:28 -0400

    Create shutdowncomputer.sh

---
+++ shutdowncomputer.sh
@@ -0,0 +1,18 @@
+# How to shutdown computer from command line.
+# Need to use sudo which means superuser do.
+
+sudo shutdown /s # To shutdown computer.
+
+sudo shutdown /l # To log off your computer.
+
+sudo shutdown now # To shutdown right now.
+
+sudo shutdown /r # To restart computer.
+
+sudo shutdown /? # To get a complete list of shutdown options.
+
+sudo reboot # Another way to shutdown.
+
+sudo halt # Instructs the hardware to stop all CPU functions.
+
+sudo poweroff # Sends an APCI signal which instructs the system to power down.
