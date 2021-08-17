56dd34981270ced4b1a201c3444a9ff2076fe52d
D. W <owl.robot@aol.com>
2021-08-16 20:16:34 -0400

    Update traceroute.sh

--- traceroute.sh
+++ traceroute.sh
@@ -5,10 +5,12 @@ sudo apt-get install traceroute # To install traceroute on Ubuntu.
 
 traceroute google.com # Eliciting a response at each hop on it's way to a destination.
 
-traceroute -n google.com # Hiding device names to cut out some of the clutter to see the data.
+traceroute -n google.com # Hiding device names to cut out some of the clutter to see the data. Useful if DNS is not working properly.
 
 traceroute -w 7.0 google.com # Changing the timeout value, the default is 5 seconds. This changes it to 7 seconds.
 
 traceroute -q 1 google.com # Changing the number of packets/queries to 1 instead of the default 3.
 
 traceroute -f 10 google.com # If you want to skip hops. 
+
+traceroute -F google.com # To not fragment the packet if it's too big for a network hop's MTU (Maximum Transition Unit).
